eb018dcf4770666544fe8c423a7318af304219a4
Manuel Schnabel <mschnabel1992@gmail.com>
2023-10-20 10:08:42 +0200

    [TASK] Add dependencies for non composer installations

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,9 @@ $EM_CONF[$_EXTKEY] = [
   'version' => '1.0.0',
   'constraints' => [
     'depends' => [
-      'typo3' => '11.5.0-12.4.99',
+        'php' => '>=7.4.0',
+        'typo3' => '11.5.0-12.4.99',
+        'mailjet/mailjet-apiv3-php' => '~1.6'
     ],
     'conflicts' => [],
     'suggests' => [],
